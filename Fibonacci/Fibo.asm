30cd4ba6d953b2e410b38fac201083b5afbf56fc
TopDevBln <top.dev.bln@gmail.com>
2024-08-16 18:05:03 +0300

    fobo

--- Fibonacci/Fibo.asm
+++ Fibonacci/Fibo.asm
@@ -1,74 +1,124 @@
 section .bss
-    num resb 1        ; Reserve one byte for the input number
+    num resb 1     
+    ascii_char resb 1 
+
+section .data   
+    newline db 0x0A   
+    fib1 dw 1
+    fib2 dw 1
+    sum dw 0
+    
 
 section .text
     global _start
 
+
 _start:
-    ; Read a single character from stdin
-    mov rax, 0        ; syscall number for sys_read
-    mov rdi, 0        ; file descriptor 0 (stdin)
-    mov rsi, num      ; address of buffer to store input
-    mov rdx, 1        ; number of bytes to read
+
+    mov rax, 0        
+    mov rdi, 0       
+    mov rsi, num     
+    mov rdx, 1       
     syscall
 
-    ; Check if input is valid ASCII character
-    cmp byte [num], '0'
-    jb invalid_input  ; If input is less than '0', exit
-    cmp byte [num], '9'
-    ja invalid_input  ; If input is greater than '9', exit
+    
+    movzx rax, byte [num] 
+    sub rax, '0'     
 
-    ; Convert the ASCII character to an integer
-    movzx rax, byte [num] ; Load the byte from buffer into rax
-    sub rax, '0'      ; Convert ASCII character to integer ('0' = 48 in ASCII)
 
-    ; Store the number in rbx
     mov rbx, rax
 
-    ; Initialize the counter
-    mov rcx, 1        ; Counter starts from 1
 
-print_loop:
-    ; Check if rcx > rbx
-    cmp rcx, rbx
-    jg done           ; If counter > number, exit loop
+    mov rcx, 1  
 
-    ; Print the counter (convert number to ASCII)
-    mov rax, 1        ; syscall number for sys_write
-    mov rdi, 1        ; file descriptor 1 (stdout)
-    mov rdx, 1        ; number of bytes to write
-    add rcx, '0'      ; Convert integer to ASCII
-    mov [num], cl     ; Store it in buffer
-    mov rsi, num      ; Address of buffer to print
+    cmp rbx, 1
+    jl exit
+
+init:
+    movzx rax, word [fib1]
+    add rax, '0'
+    mov [ascii_char], al
+
+    mov rdi, 1
+    mov rsi, ascii_char
+    mov rdx, 1
+    mov rax, 1
+    syscall
+    cmp rbx, 1
+    je exit
+
+    
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    mov rax, 1
     syscall
 
-    ; New line (print '\n')
-    mov rax, 1        ; syscall number for sys_write
-    mov rdi, 1        ; file descriptor 1 (stdout)
-    mov rsi, newline  ; Address of newline character
-    mov rdx, 1        ; number of bytes to write
+    movzx rax, word [fib2]
+    add rax, '0'
+    mov [ascii_char], al
+    dec rbx
+
+    mov rdi, 1
+    mov rsi, ascii_char
+    mov rdx, 1
+    mov rax, 1
     syscall
 
-    ; Increment the counter
-    mov rcx, [num]
-    sub rcx, '0'
-    inc rcx
-    mov [num], cl
-
-    ; Repeat the loop
-    jmp print_loop
-
-done:
-    ; Exit the program
-    mov rax, 60       ; syscall number for sys_exit
-    xor rdi, rdi      ; exit code 0
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    mov rax, 1
     syscall
 
-invalid_input:
-    ; Exit the program with invalid input error code
-    mov rax, 60       ; syscall number for sys_exit
-    mov rdi, 1        ; exit code 1 (invalid input)
+    dec rbx
+   
+  
+    
+
+
+fib:
+    cmp rbx, 0
+    je exit
+
+    mov rax, [fib1]
+    add rax, [fib2]
+    mov [sum], ax
+
+    mov ax, [fib2]
+    mov [fib1], ax
+    
+    mov ax, [sum]
+    mov [fib2], ax
+
+
+
+    movzx rax, word [sum]
+    add rax, '0'
+    mov [ascii_char], al
+
+    mov rdi, 1
+    mov rsi, ascii_char
+    mov rdx, 1
+    mov rax, 1
     syscall
 
-section .data
-newline db 0xA      ; Newline character
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    mov rax, 1
+    syscall
+
+    dec rbx
+    jmp fib
+
+
+
+
+
+
+
+exit:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
